b1d5479a1ac5a7d849c732b6f4788dc5a6e8c124
willianenf <williane010201@gmail.com>
2022-11-12 17:24:10 -0300

    update carousel e responsivo

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
+$(document).ready(function () {
+    
+    $("#owl-demo").owlCarousel({
+        navigation: true, // Show next and prev buttons
+         slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true,
+        items: 5
+        
+    });
+});
+$(document).ready(function() {
+   
+   $("#owl-example").owlCarousel({
+   
+    loop: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout:2000,
+    margin: 10,
     responsive:{
         0:{
-            items:2
+           items:1
         },
         600:{
             items:3
         },
         1000:{
-            items:5
+         items:5
         }
-    }
-})
+   }
+   });
+  
+ });
